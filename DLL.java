3f313a150204a4d28fbc8f5b58fb9b32e31b2e48
Rahul Chauhan <rahulchauhanmalnath@gmail.com>
2023-10-16 22:18:25 +0530

    Add files via upload

---
+++ DLL.java
@@ -0,0 +1,89 @@
+package Linked_List;
+
+public class DLL {
+
+   private Node head;
+    public void insertFirst(int val){
+        Node node= new Node(val);
+        node.next=head;
+        node.prev=null;
+        if(head!=null){
+            head.prev=node;
+        }
+        head=node;
+    }
+    public void insertLast(int val){
+        Node node= new Node(val);
+        node.next=null;
+        Node last=head;
+
+        if(head==null){
+            node.prev=null;
+            head=node;
+            return;
+        }
+        while(last.next!=null){
+            last=last.next;
+        }
+        last.next=node;
+        node.prev=last;
+        last=node;
+    }
+    public Node find(int value){
+        Node node=head;
+        while(node!=null){
+            if(node.val==value){
+                return node;
+            }
+            node=node.next;
+        }
+        return null;
+    }
+    public void insert(int after, int val){
+        Node p=find(after);
+        if(p==null){
+            System.out.println("does not exist");
+            return;
+        }
+        Node node = new Node(val);
+        node.next=p.next;
+        p.next=node;
+        node.prev=p;
+        if(node.next!=null){
+            node.next.prev=node;
+        }
+
+    }
+    public void display(){
+        Node node=head;
+        Node last=null;
+        while(node!=null){
+            System.out.print(node.val+ "->");
+            last=node;
+            node=node.next;
+        }
+        System.out.println("end");
+
+        System.out.println("print in reverse");
+        while (last!=null){
+            System.out.print(last.val+ "->");
+            last=last.prev;
+        }
+        System.out.println("start");
+    }
+    private class Node{
+        int val;
+        Node prev;
+        Node next;
+
+        public Node(int val){
+            this.val=val;
+        }
+        public Node(int val, Node next, Node prev){
+            this.val=val;
+            this.next=next;
+            this.prev=prev;
+        }
+    }
+
+}
